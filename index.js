9e4e81d100694b50336a25dd93000fd5e3581129
Sahil Kumar <sahil511kumar@gmail.com>
2024-06-22 02:48:37 +0530

    add css and basic functionality

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
 const buttonWrapper = document.querySelector(".buttonWrapper");
+const prevExp = document.querySelector("prevExp");
+const currentAns = document.querySelector("currentAns");
 
 document.addEventListener("DOMContentLoaded", () => {
   generateButtons();
 });
 
+buttonWrapper.addEventListener("click", ({ target: { value } }) => {
+  if (!value) return;
+  console.info({ value });
+});
+
 const generateButtons = () => {
-  buttonsArray.forEach((key) => {
-    const button = document.createElement("button");
-    button.textContent = key;
-    buttonWrapper.appendChild(button);
+  buttonsArray.forEach((rowArr, i) => {
+    const rowEl = document.createElement("div");
+    rowEl.classList.add("buttonRow");
+    rowArr.forEach((key, j) => {
+      const button = document.createElement("button");
+      let id;
+      if (i === 0 || j === rowArr.length - 1) id = "operator";
+      else if (key === "=") id = "equals";
+      else id = "operand";
+      button.textContent = key;
+      button.value = key;
+      button.id = id;
+      rowEl.appendChild(button);
+    });
+    buttonWrapper.appendChild(rowEl);
   });
 };
 
 const buttonsArray = [
-  "(",
-  ")",
-  "%",
-  "AC",
-  7,
-  8,
-  9,
-  "/",
-  4,
-  5,
-  6,
-  "x",
-  1,
-  2,
-  3,
-  "-",
-  0,
-  ".",
-  "=",
-  "+",
+  ["+/-", "%", "CE", "AC"],
+  [7, 8, 9, "/"],
+  [4, 5, 6, "x"],
+  [1, 2, 3, "-"],
+  [0, ".", "=", "+"],
 ];
